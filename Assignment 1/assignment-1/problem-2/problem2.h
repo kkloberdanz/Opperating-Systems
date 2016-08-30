8beb950850e78d607bde731c1b8b774f9b05f115
kkloberdanz <kyle.g.kloberdanz@gmail.com>
2016-08-29 21:34:51 -0500

    a1 p2 kind of working

---
+++ Assignment 1/assignment-1/problem-2/problem2.h
@@ -0,0 +1,10 @@
+#ifndef __PROBLEM2_H__
+#define __PROBLEM2_H__
+
+#define MAX_NUM_LENGTH 10000
+
+void __add_big_ints__(const int *, const int *, int *, int *);
+
+void add_and_print(char*, char*);
+
+#endif /* __PROBLEM2_H__ */
